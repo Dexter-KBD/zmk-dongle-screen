46509b432563ecb17cb672d99d7664ead5a62ac6
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-10-10 13:32:22 +0900

    Update output_status.h

--- boards/shields/dongle_screen/src/widgets/output_status.h
+++ boards/shields/dongle_screen/src/widgets/output_status.h
@@ -4,29 +4,22 @@
  * SPDX-License-Identifier: MIT
  */
 
-#ifndef ZMK_WIDGET_OUTPUT_STATUS_H
-#define ZMK_WIDGET_OUTPUT_STATUS_H
+#ifndef OUTPUT_STATUS_H
+#define OUTPUT_STATUS_H
 
 #include <zephyr/kernel.h>
-#include <lvgl.h>
 #include <zmk/display.h>
-#include <zmk/event_manager.h>
-#include <zmk/events/endpoint_changed.h>
-#include <zmk/events/ble_active_profile_changed.h>
-#include <zmk/events/usb_conn_state_changed.h>
-#include <zmk/usb.h>
-#include <zmk/ble.h>
 #include <zmk/endpoints.h>
+#include <lvgl.h>
 
 struct zmk_widget_output_status {
     lv_obj_t *obj;
-
-    lv_obj_t *transport_label; // USB/ BLE 표시 라벨
-    lv_obj_t *ble_label;       // BLE 프로필 인덱스 표시
-
-    sys_snode_t node;          // linked list 노드
+    lv_obj_t *transport_label;  // USB/BLE 텍스트
+    lv_obj_t *ble_label;        // BLE 프로필 번호
+    sys_snode_t node;
 };
 
+// 상태 구조체 정의는 헤더에서만
 struct output_status_state {
     struct zmk_endpoint_instance selected_endpoint;
     int active_profile_index;
@@ -35,7 +28,10 @@ struct output_status_state {
     bool usb_is_hid_ready;
 };
 
+// 위젯 초기화
 int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent);
+
+// LVGL 오브젝트 반환
 lv_obj_t *zmk_widget_output_status_obj(struct zmk_widget_output_status *widget);
 
-#endif /* ZMK_WIDGET_OUTPUT_STATUS_H */
+#endif // OUTPUT_STATUS_H
