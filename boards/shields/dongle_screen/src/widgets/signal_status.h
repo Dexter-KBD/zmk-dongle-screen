1c8a278abc4a2405286b10585dbf7fcf21f48d06
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-10-11 16:04:46 +0900

    Update signal_status.h

--- boards/shields/dongle_screen/src/widgets/signal_status.h
+++ boards/shields/dongle_screen/src/widgets/signal_status.h
@@ -9,11 +9,15 @@
 #include <zephyr/kernel.h>
 #include <lvgl.h>
 
+// Signal Status Widget 구조체
 struct zmk_widget_signal_status {
-    lv_obj_t *obj;
-    lv_obj_t *label;
-    sys_snode_t node;
+    lv_obj_t *obj;     // 위젯 객체
+    lv_obj_t *label;   // 라벨 객체
+    sys_snode_t node;  // 싱글 링크드 리스트 노드
 };
 
+// 초기화 함수
 int zmk_widget_signal_status_init(struct zmk_widget_signal_status *widget, lv_obj_t *parent);
+
+// 위젯 객체 반환
 lv_obj_t *zmk_widget_signal_status_obj(struct zmk_widget_signal_status *widget);
