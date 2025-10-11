ed1ec762a29d8e70f5a1d1bf8bc02982e7cc5bbb
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-10-11 18:09:11 +0900

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -54,7 +54,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_OUTPUT_ACTIVE
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 10, 10);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 20, 10);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_BATTERY_ACTIVE
