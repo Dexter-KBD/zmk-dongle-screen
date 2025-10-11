29053f1bbee97b5f2617e88850801ba70b21947c
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-10-11 17:52:00 +0900

    Update custom_status_screen.c

--- boards/shields/dongle_screen/src/custom_status_screen.c
+++ boards/shields/dongle_screen/src/custom_status_screen.c
@@ -68,7 +68,7 @@ lv_obj_t *zmk_display_status_screen()
 
 #if CONFIG_DONGLE_SCREEN_WPM_ACTIVE
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
-    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 20, 40);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_TOP_LEFT, 20, 20);
 #endif
 
 #if CONFIG_DONGLE_SCREEN_LAYER_ACTIVE
@@ -79,11 +79,6 @@ lv_obj_t *zmk_display_status_screen()
 #if CONFIG_DONGLE_SCREEN_MODIFIER_ACTIVE
     zmk_widget_mod_status_init(&mod_widget, screen);
     lv_obj_align(zmk_widget_mod_status_obj(&mod_widget), LV_ALIGN_CENTER, 0, 35);
-#endif
-
-#if CONFIG_DONGLE_SCREEN_SIGNAL_ACTIVE
-    zmk_widget_signal_status_init(&signal_status_widget, screen);
-    lv_obj_align(zmk_widget_signal_status_obj(&signal_status_widget), LV_ALIGN_TOP_LEFT, 20, 20);
 #endif
 
     return screen;
