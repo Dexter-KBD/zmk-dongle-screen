ee20f4bf77ebcd376d51759bd47543ce003457c4
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-10-12 18:58:18 +0900

    Update wpm_status.c

--- boards/shields/dongle_screen/src/widgets/wpm_status.c
+++ boards/shields/dongle_screen/src/widgets/wpm_status.c
@@ -92,7 +92,7 @@ int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *p
     // "WPM" 텍스트
     widget->wpm_label_title = lv_label_create(widget->obj);
     lv_label_set_text(widget->wpm_label_title, "WPM");
-    lv_obj_set_style_text_color(widget->wpm_label_title, lv_color_from_web("#DCEDC1"), 0); // 흰색
+    lv_obj_set_style_text_color(widget->wpm_label_title, lv_color_from_web("#00CEC9"), 0); // wpm글자색
     lv_obj_set_style_text_align(widget->wpm_label_title, LV_TEXT_ALIGN_LEFT, 0);
     lv_obj_align(widget->wpm_label_title, LV_ALIGN_TOP_LEFT, 0, 0);
 
