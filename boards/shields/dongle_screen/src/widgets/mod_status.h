b2bf9d46629c6e7f5c2a361284368812b8fb7738
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-11-06 09:58:20 +0900

    Update mod_status.h

--- boards/shields/dongle_screen/src/widgets/mod_status.h
+++ boards/shields/dongle_screen/src/widgets/mod_status.h
@@ -2,13 +2,13 @@
 
 #include <lvgl.h>
 #include <zephyr/kernel.h>
+#include <zmk/events/keyboard_state_changed.h>
 
-// mod_status 위젯 구조체
 struct zmk_widget_mod_status {
     sys_snode_t node;
-    lv_obj_t *obj;
+    lv_obj_t *mod_label;
+    lv_obj_t *caps_label;
 };
 
-// 초기화 및 객체 접근 함수 선언
 int zmk_widget_mod_status_init(struct zmk_widget_mod_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_mod_status_obj(struct zmk_widget_mod_status *widget);
