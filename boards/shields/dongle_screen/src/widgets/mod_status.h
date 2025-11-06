afe4ed2c4d6d2ad2b50cbac75f574d510213d3cf
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-11-06 09:22:01 +0900

    Update mod_status.h

--- boards/shields/dongle_screen/src/widgets/mod_status.h
+++ boards/shields/dongle_screen/src/widgets/mod_status.h
@@ -1,12 +1,11 @@
 #pragma once
-
 #include <lvgl.h>
 #include <zephyr/kernel.h>
 
 struct zmk_widget_mod_status {
-    sys_snode_t node;   // Zephyr 싱글 리스트용 노드
-    lv_obj_t *obj;      // LVGL 컨테이너 객체
-    lv_obj_t *label;    // LVGL 라벨 객체
+    sys_snode_t node;
+    lv_obj_t *obj;
+    lv_obj_t *label;
 };
 
 int zmk_widget_mod_status_init(struct zmk_widget_mod_status *widget, lv_obj_t *parent);
