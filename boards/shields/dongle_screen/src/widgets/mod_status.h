9ac46923ba7897ef8ceccef0e9d99b57da78fa33
Dexter <55927478+Dexter-KBD@users.noreply.github.com>
2025-11-06 09:05:49 +0900

    Update mod_status.h

--- boards/shields/dongle_screen/src/widgets/mod_status.h
+++ boards/shields/dongle_screen/src/widgets/mod_status.h
@@ -2,9 +2,12 @@
 
 #include <lvgl.h>
 #include <zmk/display.h>
+#include <zephyr/sys/slist.h>  // sys_snode_t용
 
-struct zmk_widget_mod_status
-{
+/* ===========================
+   모드 상태 위젯
+   =========================== */
+struct zmk_widget_mod_status {
     sys_snode_t node;
     lv_obj_t *obj;
     lv_obj_t *label;
@@ -12,3 +15,14 @@ struct zmk_widget_mod_status
 
 int zmk_widget_mod_status_init(struct zmk_widget_mod_status *widget, lv_obj_t *parent);
 lv_obj_t *zmk_widget_mod_status_obj(struct zmk_widget_mod_status *widget);
+
+/* ===========================
+   Caps Word 상태 위젯
+   =========================== */
+struct zmk_widget_caps_word_indicator {
+    sys_snode_t node;
+    lv_obj_t *obj;
+};
+
+int zmk_widget_caps_word_indicator_init(struct zmk_widget_caps_word_indicator *widget, lv_obj_t *parent);
+lv_obj_t *zmk_widget_caps_word_indicator_obj(struct zmk_widget_caps_word_indicator *widget);
